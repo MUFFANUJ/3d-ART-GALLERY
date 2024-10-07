2eed116b54bb1e1bd078e58dd82c0a55da21f819
Mehak261124 <145124929+Mehak261124@users.noreply.github.com>
2024-10-07 13:49:24 +0530

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -15,9 +15,7 @@ if (!PUBLISHABLE_KEY) {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <App />
     </ClerkProvider>
-  </React.StrictMode>
 );
