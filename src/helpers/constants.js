18026e51cf8d8b55d6c10ec9549250e8352391b6
Mehak261124 <145124929+Mehak261124@users.noreply.github.com>
2024-11-08 02:46:16 +0530

    ok

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,7 +1,7 @@
 let ENDPOINT = "";
 
 if (process.env.NODE_ENV === "production") {
-  console.log(process.enev.NODE_ENV);
+  // console.log(process.enev.NODE_ENV);
   ENDPOINT = "https://threed-art-gallery-backend.onrender.com";
 } else if (process.env.NODE_ENV === "development") {
   ENDPOINT = "http://localhost:3005"; 
