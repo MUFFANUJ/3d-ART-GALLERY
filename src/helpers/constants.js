7166d2f0f5442a8b00f5276ecff06e4b7dec647a
Mehak261124 <145124929+Mehak261124@users.noreply.github.com>
2024-11-08 15:28:40 +0530

    updated endpoint

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,9 +1,9 @@
 let ENDPOINT = "";
 
-// if (process.env.NODE_ENV === "production") {
-//   ENDPOINT = "https://threed-art-gallery-backend.onrender.com";
-// } else if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
+  ENDPOINT = "https://threed-art-gallery-backend.onrender.com";
+} else if (process.env.NODE_ENV === "development") {
   ENDPOINT = "http://localhost:3005"; 
-// }
+}
 
 export default ENDPOINT;
